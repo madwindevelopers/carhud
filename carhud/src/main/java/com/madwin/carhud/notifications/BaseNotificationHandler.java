53a88e62c073be3bad7d1f46c5fd3cfd2f133a22
Andrew <a.rohde89@gmail.com>
2014-06-07 08:06:26 -0600

    fix notification null pointers

--- carhud/src/main/java/com/madwin/carhud/notifications/BaseNotificationHandler.java
+++ carhud/src/main/java/com/madwin/carhud/notifications/BaseNotificationHandler.java
@@ -9,7 +9,6 @@ import android.service.notification.StatusBarNotification;
 import android.util.Log;
 
 import com.madwin.carhud.MainActivity;
-import com.madwin.carhud.debug.DebuggingHandler;
 
 /**
  * Created by Andrew on 5/28/2014.
@@ -38,13 +37,25 @@ public class BaseNotificationHandler {
 
         extras.putString("notificationtype", "notification");
         extras.putString("packagename", sbn.getPackageName());
-        extras.putString("packagelabel", packagelabel);
-        extras.putString("notificationtitle", sbn.getNotification().extras.get(
-                Notification.EXTRA_TITLE).toString());
-        extras.putString("notificationtext", sbn.getNotification().extras.get(
-                Notification.EXTRA_TEXT).toString());
-        extras.putString("notificationsubtext", sbn.getNotification().extras.get(
-                Notification.EXTRA_SUB_TEXT).toString());
+
+        if (sbn.getNotification().extras.get(Notification.EXTRA_TITLE) != null) {
+            extras.putString("notificationtitle", sbn.getNotification().extras.get(
+                    Notification.EXTRA_TITLE).toString());
+        } else {
+            extras.putString("notificationtitle", "");
+        }
+        if (sbn.getNotification().extras.get(Notification.EXTRA_TEXT) != null) {
+            extras.putString("notificationtext", sbn.getNotification().extras.get(
+                    Notification.EXTRA_TEXT).toString());
+        } else {
+            extras.putString("notificationtext", "");
+        }
+        if (sbn.getNotification().extras.get(Notification.EXTRA_SUB_TEXT) != null) {
+            extras.putString("notificationsubtext", sbn.getNotification().extras.get(
+                    Notification.EXTRA_SUB_TEXT).toString());
+        } else {
+            extras.putString("notificationsubtext", "");
+        }
 
         Intent intent = new Intent("com.madwin.carhud.NOTIFICATION_LISTENER");
         intent.putExtras(extras);
