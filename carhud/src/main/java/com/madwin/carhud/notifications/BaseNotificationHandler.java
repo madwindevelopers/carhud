726ea2798c814ea8cc1adc8df6c3cfee5c8df6e8
Andrew Rohde <a.rohde89@gmail.com>
2015-02-19 10:40:00 -0600

    forgot to delete commented code

--- carhud/src/main/java/com/madwin/carhud/notifications/BaseNotificationHandler.java
+++ carhud/src/main/java/com/madwin/carhud/notifications/BaseNotificationHandler.java
@@ -34,56 +34,6 @@ public class BaseNotificationHandler extends Handler {
         } else {
             nf.setNotificationSubText("");
         }
-
-
-//
-//
-//
-//        Log.e(TAG, "Entered BaseHandler");
-//        if (MainActivity.getAppContext().getPackageManager() != null && MainActivity.activityRunning) {
-//            String packagelabel = "";
-//            PackageManager pm = MainActivity.getAppContext().getPackageManager();
-//            Log.d(TAG, "Notification package name = " + sbn.getPackageName());
-//            assert pm != null;
-//            try {
-//                ApplicationInfo ai = pm.getApplicationInfo(sbn.getPackageName(), 0);
-//                if (ai.loadLabel(pm) != null) {
-//                    packagelabel = ai.loadLabel(pm).toString();
-//                }
-//            } catch (PackageManager.NameNotFoundException e) {
-//                Log.d(TAG, "Unable to get packagelabel in BaseNotificationHandler");
-//                e.printStackTrace();
-//            }
-//
-//            Bundle extras = new Bundle();
-//
-//            //extras.putString("notificationtype", "notification");
-//            extras.putString(NotificationFragment.PACKAGE_NAME, sbn.getPackageName());
-//            extras.putString(NotificationFragment.PACKAGE_LABEL, packagelabel);
-//
-//            if (sbn.getNotification().extras.get(Notification.EXTRA_TITLE) != null) {
-//                extras.putString(NotificationFragment.TITLE, sbn.getNotification().extras.get(
-//                        Notification.EXTRA_TITLE).toString());
-//            } else {
-//                extras.putString(NotificationFragment.TITLE, "");
-//            }
-//            if (sbn.getNotification().extras.get(Notification.EXTRA_TEXT) != null) {
-//                extras.putString(NotificationFragment.TEXT, sbn.getNotification().extras.get(
-//                        Notification.EXTRA_TEXT).toString());
-//            } else {
-//                extras.putString(NotificationFragment.TEXT, "");
-//            }
-//            if (sbn.getNotification().extras.get(Notification.EXTRA_SUB_TEXT) != null) {
-//                extras.putString(NotificationFragment.SUBTEXT, sbn.getNotification().extras.get(
-//                        Notification.EXTRA_SUB_TEXT).toString());
-//            } else {
-//                extras.putString(NotificationFragment.SUBTEXT, "");
-//            }
-//
-//            Intent intent = new Intent(NotificationFragment.NOTIFICATION_INTENT);
-//            intent.putExtras(extras);
-//            MainActivity.getAppContext().sendBroadcast(intent);
-//        }
     }
 
     @Override
