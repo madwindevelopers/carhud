5f9dae4b112b3bb54cb4e81bd1f0fef945a9bb56
Andrew Rohde <a.rohde89@gmail.com>
2015-02-25 19:15:06 -0600

    more logical filename

--- carhud/src/main/java/com/madwin/carhud/AppListArrayAdapter.java
+++ carhud/src/main/java/com/madwin/carhud/AppListArrayAdapter.java
@@ -20,7 +20,7 @@ public class AppListArrayAdapter extends ArrayAdapter<String> {
     private final ArrayList<Drawable> app_icon;
 
     public AppListArrayAdapter(Context context, ArrayList<String> app_name, ArrayList<Drawable> app_icon) {
-        super(context, R.layout.custom_list_item, app_name);
+        super(context, R.layout.application_list_item, app_name);
         this.context = context;
         this.app_name = app_name;
         this.app_icon = app_icon;
@@ -30,7 +30,7 @@ public class AppListArrayAdapter extends ArrayAdapter<String> {
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View rowView = inflater.inflate(R.layout.custom_list_item, parent, false);
+        View rowView = inflater.inflate(R.layout.application_list_item, parent, false);
         ImageView imageView = (ImageView) rowView.findViewById(R.id.app_image_item);
         TextView textView = (TextView) rowView.findViewById(R.id.app_name_item);
         textView.setText(app_name.get(position));
